16d7ad1b936439d8d505c058af8c7ba52b8da1dd
Alaa-Khaled12 <166175261+Alaa-Khaled12@users.noreply.github.com>
2025-05-02 15:51:36 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package tictactoe;
+
+import tictactoe.view.MainMenuView;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            new MainMenuView();
+        });
+    }
+}
