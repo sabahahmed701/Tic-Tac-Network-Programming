50c8f6071ceb643765a0dd1bafa1f33aa93d3210
Shrouk3dnan11 <148492034+Shrouk3dnan11@users.noreply.github.com>
2025-05-02 15:29:24 +0300

    Add files via upload

---
+++ network/Client.java
@@ -0,0 +1,32 @@
+package tictactoe.network;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public Client(String host, int port) throws IOException {
+        socket = new Socket(host, port);
+        setupStreams();
+    }
+
+    private void setupStreams() throws IOException {
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public void send(String message) {
+        out.println(message);
+    }
+
+    public String receive() throws IOException {
+        return in.readLine();
+    }
+
+    public void close() throws IOException {
+        socket.close();
+    }
+}
